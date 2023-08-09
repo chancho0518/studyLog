42375556bc20f9ef20d82716dc45a804e815d93c
이충열 <chancho0518@gmail.com>
2023-08-09 11:46:17 +0900

    List 예제코드 작성

---
+++ 50. Java/JavaPractice/JavaOOP/src/listexem/exercise/ListTest.java
@@ -0,0 +1,29 @@
+package listexem.exercise;
+
+import java.util.ArrayList;
+
+public class ListTest {
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("apple");
+		list.add("banana");
+		list.add("cherry");
+		list.add("orange");
+		list.add("banana");
+		
+		System.out.println(list.get(0));
+		System.out.println(list.get(1));
+		System.out.println(list.get(2));
+		System.out.println(list.get(3));
+		System.out.println(list.get(list.size()-1));
+		
+		list.remove(0);
+		list.set(1, "peach");
+		
+		for(String fruit : list) {
+			System.out.println(fruit);
+		}
+		
+		System.out.println(list.get(4));
+	}
+}
