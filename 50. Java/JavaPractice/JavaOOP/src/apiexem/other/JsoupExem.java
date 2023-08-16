712ffe28e91fdd592182006d93ce974a7443c9e7
이충열 <chancho0518@gmail.com>
2023-08-16 14:01:21 +0900

    Jsoup 연습

---
+++ 50. Java/JavaPractice/JavaOOP/src/apiexem/other/JsoupExem.java
@@ -0,0 +1,35 @@
+package apiexem.other;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+public class JsoupExem {
+	public static void main(String[] args) {
+		String url = "https://sum.su.or.kr:8888/bible/today";
+		
+		try {
+			Document document = Jsoup.connect(url).get();	
+			
+			Element bibleText = document.getElementById("bible_text");
+			Element date = document.getElementById("dailybible_info");
+			
+			Elements num = document.select(".num");
+			Elements info = document.select(".info");
+			
+			System.out.println("제목: " + bibleText.text());
+			System.out.println("날짜: " + date.text());
+			
+			for(int i=0; i<num.size(); i++) {
+				System.out.println(num.get(i).text() + ". " +  info.get(i).text());
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
