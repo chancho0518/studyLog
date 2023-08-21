deb2e144db754f954d70cff176f03dcdc63e5f4e
이충열 <chancho0518@gmail.com>
2023-08-21 15:50:49 +0900

    OpenWeatherMapAPI 연습

---
+++ 50. Java/JavaPractice/JavaOOP/src/apiexem/other/OpenAPIExem.java
@@ -0,0 +1,52 @@
+package apiexem.other;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class OpenAPIExem {
+
+	public static void main(String[] args) {
+		String apiKey= "d78e07fa8bb6a37fa7cef96816649838";
+		String city = "Singapore";
+		String urlString = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&lang=kr&appid=" + apiKey + "&units=metric";
+		
+		try {
+			URL url = new URL(urlString);
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Accept", "application/json");
+			
+			int responseCode = connection.getResponseCode();
+			
+			if(responseCode ==  200) {
+				// Stream(입력 / 출력)의 연결
+				BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+				String inputLine;
+				StringBuffer content = new StringBuffer();
+				
+				while ((inputLine=input.readLine()) != null) {
+					content.append(inputLine);
+				}
+				
+				input.close();
+				
+				System.out.println("Content.toString() = " + content.toString());
+				JsonObject weatherData = JsonParser.parseString(content.toString()).getAsJsonObject();
+				JsonObject mainData = weatherData.getAsJsonObject("main");
+				Double temp = mainData.get("temp").getAsDouble();
+				System.out.println("temp = " + temp);
+				connection.disconnect();
+			} else {
+				// 오류
+			}	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
