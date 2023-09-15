967401b316a35cded60e64fe6e7bd25f68cd6b7d
이충열 <chancho0518@gmail.com>
2023-09-15 17:03:02 +0900

    원격 프로그램으로 응답

--- 51. Spring/example/src/main/java/com/example/DayTeller.java
+++ 51. Spring/example/src/main/java/com/example/DayTeller.java
@@ -1,24 +1,51 @@
 package com.example;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
 
 // 년, 월, 일을 입력하면 요일을 출력
-@RestController
+@Controller
 public class DayTeller {
 
     @RequestMapping("getDay")
-    public String main(HttpServletRequest request) {
+    public void main(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        // 1. 입력
         String year = request.getParameter("year");
         String month = request.getParameter("month");
         String day = request.getParameter("day");
 
-        System.out.println("Year: " + year);
-        System.out.println("Month: " + month);
-        System.out.println("Day: " + day);
+        int yyyy = Integer.parseInt(year);
+        int mm = Integer.parseInt(month);
+        int dd = Integer.parseInt(day);
 
-        return "Monday";
+        // 2. 연산(요일 반환)
+        Calendar cal = Calendar.getInstance();  // 현재 날짜와 시간을 갖음
+        cal.clear();  // cal의 모든 필드를 초기화
+        cal.set(yyyy, mm-1, dd);  // 월(mm)은 0~11d을 반환하기 때문에 1을 뺌
+
+        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);  // 1~7을 반환(1: 일요일, 2: 월요일 ...)
+        char yoli = "일월화수목금토".charAt(dayOfWeek - 1);
+
+        // 3. 출력(연산 내용을 브라우저에 전송)
+        response.setCharacterEncoding("ms949");  // 한글 윈도우 MS 949
+
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println(    "Year: " + year + "<br>");
+        out.println(    "Month: " + month  + "<br>");
+        out.println(    "Day: " + day  + "<br>");
+        out.println(    "Yoli: " + yoli  + "<br>");
+        out.println("</body>");
+        out.println("</html>");
     }
 }
